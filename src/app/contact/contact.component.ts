37c04f6092e83a44492530456ac2b9281240aba2
skhacker105 <skhacker105@gmail.com>
2024-01-05 14:28:12 +0530

    basic routing completed between login, home, contact and main

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { DynamicLayoutComponent } from 'shop-folder-component';
+import { DynamicLayoutComponent, TableViewComponent } from 'shop-folder-component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [DynamicLayoutComponent],
+  imports: [DynamicLayoutComponent, TableViewComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
