98fbe54cf22a6e99c70fbacd15cbb5b70ea8ba93
skhacker105 <skhacker105@gmail.com>
2024-01-06 19:18:24 +0530

    fixed swipe action

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
 import { DynamicLayoutComponent, TableViewComponent } from 'shop-folder-component';
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [DynamicLayoutComponent, TableViewComponent],
+  imports: [DynamicLayoutComponent, TableViewComponent, MatIconModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
+  rowData: any[] = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+  ];
+  columnDefs: ColDef[] = [
+    { headerName: 'Make', field: 'make', cellClass: 'default-col-width' }
+  ];
 }
